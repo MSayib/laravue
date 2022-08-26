22fd39825f2ce20d9042f33708c853a67bb819ae
MSayib <sayib.roziq2@gmail.com>
2022-08-26 14:11:54 +0700

    add README, create first API, migrations and models

---
+++ app/Http/Controllers/Post/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Note;
+use App\Models\Subject;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    public function store(Request $request)
+    {
+        $subject = Subject::findOrFail($request->subject);
+        $note = Note::create([
+            'subject_id' => $subject->id,
+            'title' => $request->title,
+            'slug' => Str::slug($request->title),
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'message' => 'Note created.',
+            'notes' => $note,
+        ]);
+    }
+}
